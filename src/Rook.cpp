4a0151799e783f2859681ab9c133e158c4e156c2
L-yz16 <yuzhelin1116@gmail.com>
2024-11-16 20:23:30 -0800

    Added Rook files and the generatePossibleMoves is able to generate moves without considering other pieces

---
+++ src/Rook.cpp
@@ -0,0 +1,39 @@
+#include "../header/Rook.hpp"
+
+Rook::Rook(std::string Character_Color) {
+  this->Character_Type =ROOK;
+  this->Character_Color = Character_Color;
+}
+
+bool Rook::getMovedStatus() const {
+  return this->Character_Moved;
+}
+
+void Rook::setMoved() {
+  this->Character_Moved = true;
+}
+
+std::vector<std::string>* Rook::generatePossibleMoves(int x, int y) { 
+  std::vector<std::string>* vec = new std::vector<std::string>();
+  std::string move;
+  // Horizontal Moves
+    for (int i=x+1; i<8; i++) {
+    move = std::to_string(i) + std::to_string(y);
+    vec->push_back(move);
+  }
+  for (int i=x-1; i>=0; i--) {
+    move = std::to_string(i) + std::to_string(y);
+    vec->push_back(move);
+  }
+  // Vertical Moves
+  for (int i=y+1; i<8; i++) {
+    move = std::to_string(x) + std::to_string(i);
+    vec->push_back(move);
+  }
+  for (int i=y-1; i>=0; i--) {
+    move = std::to_string(x) + std::to_string(i);
+    vec->push_back(move);
+  }
+
+  return vec;
+}
