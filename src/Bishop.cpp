6a499ff6255b0669c0318b93158faa9aa8276aa7
L-yz16 <yuzhelin1116@gmail.com>
2024-11-16 21:09:47 -0800

    Added Bishop files

---
+++ src/Bishop.cpp
@@ -0,0 +1,29 @@
+#include "../header/Bishop.hpp"
+
+Bishop::Bishop(std::string Character_Color) {
+  this->Character_Type =BISHOP;
+  this->Character_Color = Character_Color;
+}
+
+std::vector<std::string>* Bishop::generatePossibleMoves(int x, int y) {
+  std::vector<std::string> vec;
+  std::string move;
+  for (int i=1; i<8; i++) {
+    if (x+i<8 && y+i<8) {
+      move = std::to_string(x+i) + std::to_string(y+i);
+      vec.push_back(move);
+    }
+    if (x+i<8 && y-i>=0) {
+      move = std::to_string(x+i) + std::to_string(y-i);
+      vec.push_back(move);
+    }
+    if (x-i>=0 && y+i<8) {
+      move = std::to_string(x-i) + std::to_string(y+i);
+      vec.push_back(move);
+    } 
+    if (x-i>=0 && y-i>=0) {
+      move = std::to_string(x-i) + std::to_string(y-i);
+      vec.push_back(move);
+    }
+  }
+}
