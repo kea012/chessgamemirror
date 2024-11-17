c678f8ffae3d0e61296464dc1ac8906125dbe075
L-yz16 <yuzhelin1116@gmail.com>
2024-11-16 21:27:38 -0800

    Added return to generatePossibleMoves function.

--- src/Bishop.cpp
+++ src/Bishop.cpp
@@ -6,24 +6,25 @@ Bishop::Bishop(std::string Character_Color) {
 }
 
 std::vector<std::string>* Bishop::generatePossibleMoves(int x, int y) {
-  std::vector<std::string> vec;
+  std::vector<std::string>* vec = new std::vector<std::string>();
   std::string move;
   for (int i=1; i<8; i++) {
     if (x+i<8 && y+i<8) {
       move = std::to_string(x+i) + std::to_string(y+i);
-      vec.push_back(move);
+      vec->push_back(move);
     }
     if (x+i<8 && y-i>=0) {
       move = std::to_string(x+i) + std::to_string(y-i);
-      vec.push_back(move);
+      vec->push_back(move);
     }
     if (x-i>=0 && y+i<8) {
       move = std::to_string(x-i) + std::to_string(y+i);
-      vec.push_back(move);
+      vec->push_back(move);
     } 
     if (x-i>=0 && y-i>=0) {
       move = std::to_string(x-i) + std::to_string(y-i);
-      vec.push_back(move);
+      vec->push_back(move);
     }
   }
+  return vec;
 }
