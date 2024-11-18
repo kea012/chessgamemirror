ab422f08aee0f069ff83aa3873f35e4f93f68cc2
kea012 <kennethea12@gmail.com>
2024-11-17 17:26:43 -0800

    added namespace

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -1,5 +1,7 @@
 #include "../header/Knight.hpp"
 
+using namespace std;
+
 Knight::Knight(string characterColor) : Character(KNIGHT, characterColor) {} 
 
 std::vector<std::string>* Knight::generatePossibleMoves(int x, int y) {
