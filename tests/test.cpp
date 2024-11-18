54a5bc3a6eac3ed253fc8e01c29e098edf791255
kea012 <kennethea12@gmail.com>
2024-11-17 16:16:12 -0800

    fixed queen test

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,12 +1,22 @@
 #include "../header/Character.hpp"
 #include "../header/Pawn.hpp"
+#include "../header/Queen.hpp"
 #include "gtest/gtest.h"
 
 TEST (Pawn, MakePawn) {
 
 Pawn testPawn = Pawn("w");
 
-EXPECT_EQ(testChar.getType(), 0);
-EXPECT_EQ(testChar.getAliveStatus(), true);
-EXPECT_EQ(testChar.getColor(), "w");
+EXPECT_EQ(testPawn.getType(), 0);
+EXPECT_EQ(testPawn.getAliveStatus(), true);
+EXPECT_EQ(testPawn.getColor(), "w");
+}
+
+TEST (Queen, MakeQueen) {
+
+Queen testQueen = Queen("w");
+
+EXPECT_EQ(testQueen.getType(), 5);
+EXPECT_EQ(testQueen.getAliveStatus(), true);
+EXPECT_EQ(testQueen.getColor(), "w");
 }
