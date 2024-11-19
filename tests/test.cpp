977bbe06a118fafa3fd44ecb7d3e1249e7d1b214
kea012 <kennethea12@gmail.com>
2024-11-19 11:14:41 -0800

    added symbol to constructor tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,54 +9,60 @@
 
 TEST (Pawn, MakePawn) {
 
-Pawn testPawn = Pawn("w");
+Pawn testPawn = Pawn("w", "♟");
 
 EXPECT_EQ(testPawn.getType(), 0);
 EXPECT_EQ(testPawn.getAliveStatus(), true);
 EXPECT_EQ(testPawn.getColor(), "w");
+EXPECT_EQ(testPawn.getSymbol(), "♟");
 }
 
 TEST (King, MakeKing) {
 
-King testKing = King("w");
+King testKing = King("w", "♚");
 
 EXPECT_EQ(testKing.getType(), 1);
 EXPECT_EQ(testKing.getAliveStatus(), true);
 EXPECT_EQ(testKing.getColor(), "w");
+EXPECT_EQ(testKing.getSymbol(), "♚");
 }
 
 TEST (Knight, MakeKnight) {
 
-Knight testKnight = Knight("w");
+Knight testKnight = Knight("w", "♞");
 
 EXPECT_EQ(testKnight.getType(), 2);
 EXPECT_EQ(testKnight.getAliveStatus(), true);
 EXPECT_EQ(testKnight.getColor(), "w");
+EXPECT_EQ(testKnight.getSymbol(), "♞");
 }
 
 TEST (Bishop, MakeBishop) {
 
-Bishop testBishop = Bishop("w");
+Bishop testBishop = Bishop("w", "♝");
 
 EXPECT_EQ(testBishop.getType(), 3);
 EXPECT_EQ(testBishop.getAliveStatus(), true);
 EXPECT_EQ(testBishop.getColor(), "w");
+EXPECT_EQ(testBishop.getSymbol(), "♝");
 }
 
 TEST (Rook, MakeRook) {
 
-Rook testRook = Rook("w");
+Rook testRook = Rook("w", "♜");
 
 EXPECT_EQ(testRook.getType(), 4);
 EXPECT_EQ(testRook.getAliveStatus(), true);
 EXPECT_EQ(testRook.getColor(), "w");
+EXPECT_EQ(testRook.getSymbol(), "♜");
 }
 
 TEST (Queen, MakeQueen) {
 
-Queen testQueen = Queen("w");
+Queen testQueen = Queen("w", "♛");
 
 EXPECT_EQ(testQueen.getType(), 5);
 EXPECT_EQ(testQueen.getAliveStatus(), true);
 EXPECT_EQ(testQueen.getColor(), "w");
+EXPECT_EQ(testQueen.getSymbol(), "♛");
 }
