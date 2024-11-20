0689929455cd62eafa51dd396fe4f436436a3783
kea012 <kennethea12@gmail.com>
2024-11-20 12:49:18 -0800

    add constructor declaration

--- header/Board.hpp
+++ header/Board.hpp
@@ -14,6 +14,7 @@ class Board {
 		std::vector<Character*> whitePieces;
 		std::vector<Character*> blackPieces;
 	public:
+		Board();
 		bool isSpaceOccupied(std::string position);
 		bool isSpaceOccupied(Position pos);
 		bool hasMoves(Position piecePos);
