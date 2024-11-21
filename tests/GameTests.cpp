a633678d67f0d720e44902d37c46da114c967313
emukai3 <ethan.t.mukai@gmail.com>
2024-11-21 14:29:03 -0800

    Added GameConstructorTests

---
+++ tests/GameTests.cpp
@@ -0,0 +1,48 @@
+#include "gtest/gtest.h"
+#include "../header/Game.hpp"
+
+TEST(GameConstructorTests, DefaultOutputString) {
+    Game tGame;
+    std::string tStr = "WELCOME TO CHESS\nEnter 'S' to start a new game or 'Q' to quit the program";
+
+    EXPECT_EQ(tGame.getOutputString(), tStr);
+}
+
+TEST(GameConstructorTests, DefaultTurnState) {
+    Game tGame;
+
+    EXPECT_EQ(tGame.getTurn(), noTurn);
+}
+
+TEST(GameConstructorTests, DefaultPiecePos) {
+    Game tGame;
+    Position tPiecePos = tGame.getSelectedPiecePos();
+
+    int tRow = tPiecePos.getRow();
+    int tCol = tPiecePos.getCol();
+    std::string tPosStr = tPiecePos.getPositionString();
+
+    EXPECT_EQ(tRow, -1);
+    EXPECT_EQ(tCol, -1);
+    EXPECT_EQ(tPosStr, "--");
+}
+
+TEST(GameConstructorTests, DefaultMovePos) {
+    Game tGame;
+    Position tMovePos = tGame.getSelectedMovePos();
+
+    int tRow = tMovePos.getRow();
+    int tCol = tMovePos.getCol();
+    std::string tPosStr = tMovePos.getPositionString();
+
+    EXPECT_EQ(tRow, -1);
+    EXPECT_EQ(tCol, -1);
+    EXPECT_EQ(tPosStr, "--");
+}
+
+TEST(GameConstructorTests, DefaultBoard) {
+    Game tGame;
+    Board* tBoard = tGame.getGameBoard();
+
+    EXPECT_EQ(tBoard, nullptr);
+}
