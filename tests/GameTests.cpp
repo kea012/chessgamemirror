f6443b853651b45566f5a5fa05994859dd50d91f
emukai3 <ethan.t.mukai@gmail.com>
2024-11-21 16:31:12 -0800

    Added StateToActionTests

--- tests/GameTests.cpp
+++ tests/GameTests.cpp
@@ -90,3 +90,316 @@ TEST(UpdateTurnTests, noUpdate) {
     EXPECT_TRUE(!tGame.updateTurn());
     EXPECT_EQ(tGame.getTurn(), noTurn);
 }
+
+TEST(StateToActionTests, MenuScreenToCreateGame) {
+    Game tGame;
+    std::string expectedStr = "Starting new game...\nWhite player's turn\nEnter 'M' to make a move or 'Q' to quit game";
+    std::string tInput1 = "S";
+    std::string tInput2 = "s";
+
+    tGame.updateGameState(new MenuScreen());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new MenuScreen());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+}
+
+TEST(StateToActionTests, MenuScreenToEndProgram) {
+    Game tGame;
+    std::string expectedStr = "Ending program...";
+    std::string tInput1 = "Q";
+    std::string tInput2 = "q";
+
+    tGame.updateGameState(new MenuScreen());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new MenuScreen());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+}
+
+TEST(StateToActionTests, MenuScreenToInvalidInput) {
+    Game tGame;
+    std::string expectedStr = "Invalid input";
+    std::string tInput1 = "p";
+    std::string tInput2 = "E3";
+
+    tGame.updateGameState(new MenuScreen());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new MenuScreen());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+}
+
+TEST(StateToActionTests, EndScreenToCreateGame) {
+    Game tGame;
+    std::string expectedStr = "Starting new game...\nWhite player's turn\nEnter 'M' to make a move or 'Q' to quit game";
+    std::string tInput1 = "S";
+    std::string tInput2 = "s";
+
+    tGame.updateGameState(new EndScreen());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new EndScreen());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+}
+
+TEST(StateToActionTests, EndScreenToReturnToMenu) {
+    Game tGame;
+    std::string expectedStr = "WELCOME TO CHESS\nEnter 'S' to start a new game or 'Q' to quit the program";
+    std::string tInput1 = "Q";
+    std::string tInput2 = "q";
+
+    tGame.updateGameState(new EndScreen());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new EndScreen());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+}
+
+TEST(StateToActionTests, EndScreenToInvalidInput) {
+    Game tGame;
+    std::string expectedStr = "Invalid input";
+    std::string tInput1 = "x";
+    std::string tInput2 = "za2Al";
+
+    tGame.updateGameState(new EndScreen());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new EndScreen());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+TEST(StateToActionTests, TurnStartToSelectPiece) {
+    Game tGame;
+    std::string expectedStr = "Enter a position to select a piece";
+    std::string tInput1 = "M";
+    std::string tInput2 = "m";
+
+    tGame.updateGameState(new TurnStart());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new TurnStart());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+TEST(StateToActionTests, TurnStartToSelectPiece) {
+    Game tGame;
+    std::string expectedStr = "Enter a position to select a piece";
+    std::string tInput1 = "M";
+    std::string tInput2 = "m";
+
+    tGame.updateGameState(new TurnStart());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new TurnStart());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+// UPDATE ONCE WINNER CHECK IS IMPLEMENTED
+TEST(StateToActionTests, TurnStartToRetire) {
+    Game tGame;
+    std::string expectedStr = "Player who didn't retire won the game\nEnter 'S' to start a new game or 'Q' to return to the menu";
+    std::string tInput1 = "Q";
+    std::string tInput2 = "q";
+
+    tGame.updateGameState(new TurnStart());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new TurnStart());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+TEST(StateToActionTests, TurnStartToInvalidInput) {
+    Game tGame;
+    std::string expectedStr = "Invalid input";
+    std::string tInput1 = "S";
+    std::string tInput2 = "D7";
+
+    tGame.updateGameState(new TurnStart());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new TurnStart());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+// WILL NEED TO CREATE DIFFERENT TESTS FOR DIFFERENT POSSIBLE 'CheckPiece' OUTPUTS
+TEST(StateToActionTests, SelectingPieceToCheckPiece) {
+    Game tGame;
+    std::string expectedStr = "Enter a position to move selected piece to or 'R' to select a different piece";
+    std::string tInput1 = "A3";
+    std::string tInput2 = "H8";
+
+    tGame.updateGameState(new SelectingPiece());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new SelectingPiece());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+TEST(StateToActionTests, SelectingPieceToInvalidInput) {
+    Game tGame;
+    std::string expectedStr = "Invalid input";
+    std::string tInput1 = "A0";
+    std::string tInput2 = "D9";
+
+    tGame.updateGameState(new SelectingPiece());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new SelectingPiece());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+TEST(StateToActionTests, SelectingMoveToSelectPiece) {
+    Game tGame;
+    std::string expectedStr = "Enter a position to select a piece";
+    std::string tInput1 = "R";
+    std::string tInput2 = "r";
+
+    tGame.updateGameState(new SelectingMove());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new SelectingMove());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+// WILL NEED TO CREATE DIFFERENT TESTS FOR DIFFERENT POSSIBLE 'CheckMove' OUTPUTS
+TEST(StateToActionTests, SelectingMoveToCheckMove) {
+    Game tGame;
+    std::string expectedStr = "Enter 'C' to confirm movement or 'R' to select a different position";
+    std::string tInput1 = "A6";
+    std::string tInput2 = "F4";
+
+    tGame.updateGameState(new SelectingMove());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new SelectingMove());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+TEST(StateToActionTests, SelectingMoveToInvalidInput) {
+    Game tGame;
+    std::string expectedStr = "Invalid input";
+    std::string tInput1 = "A0";
+    std::string tInput2 = "D9";
+
+    tGame.updateGameState(new SelectingMove());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new SelectingMove());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
+
+/* Relies on Board implementation, will need to have a valid piece and valid move selected
+TEST(StateToActionTests, ConfirmingMoveToMovePiece) {
+
+}
+*/
+
+TEST(StateToActionTests, ConfirmingMoveToSelectMove) {
+    Game tGame;
+    std::string expectedStr = "Enter a position to move selected piece to";
+    std::string tInput1 = "R";
+    std::string tInput2 = "r";
+
+    tGame.updateGameState(new ConfirmingMove());
+    tGame.inputToAction(tInput1);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr);
+
+    tGame.updateGameState(new ConfirmingMove());
+    tGame.inputToAction(tInput2);
+    tGame.performCurrAction();
+    std::string tStr = tGame.getOutputString();
+    EXPECT_EQ(tStr, expectedStr); 
+}
