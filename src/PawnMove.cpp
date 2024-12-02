a3b9b9531fb4e231f7d173011337930f79bf5cb9
kea012 <kennethea12@gmail.com>
2024-12-02 11:53:34 -0800

    fixed

--- src/PawnMove.cpp
+++ src/PawnMove.cpp
@@ -7,7 +7,7 @@ PawnMove::PawnMove(string color, Board* chessBoard) : Move(PAWNMOVE, color), che
 vector<string> PawnMove::generatePossibleMoves(int row, int column) {
 
     //if white the piece moves forward down the board
-    //if (chessBoard->getPiece(row, column)->getColor() == "w") {
+    if (chessBoard->getPiece(row, column)->getColor() == "w") {
         for (int i = row + 1; i < 8; ++i) {
             if (chessBoard->getPiece(i, column) == nullptr){
                 possibleMoves.push_back(to_string(i)+to_string(column));
@@ -16,31 +16,37 @@ vector<string> PawnMove::generatePossibleMoves(int row, int column) {
                 break;
             }
         }
-    //}
+    }
 
     //diagonol down and to the right of the board
-    //if (chessBoard->getPiece(row, column)->getColor() == "w") {
+    if (chessBoard->getPiece(row, column)->getColor() == "w") {
         for (int i = row + 1, j = column + 1; (i < 8)&&(j < 8); ++i, ++j) {
-            if (chessBoard->getPiece(i, j)->getColor() != chessBoard->getPiece(row, column)->getColor()) {
+            if (chessBoard->getPiece(i, column) == nullptr){
+                break;
+            }
+            else if (chessBoard->getPiece(i, j)->getColor() != chessBoard->getPiece(row, column)->getColor()) {
                 possibleMoves.push_back(to_string(i)+to_string(j));
                 break;
             }
         }
-    //}
+    }
 
     //diagonol down and to the left of the board
-    //if (chessBoard->getPiece(row, column)->getColor() == "w") {
+    if (chessBoard->getPiece(row, column)->getColor() == "w") {
         for (int i = row + 1, j = column - 1; (i < 8)&&(j >= 8); ++i, --j) {
-            if (chessBoard->getPiece(i, j)->getColor() != chessBoard->getPiece(row, column)->getColor()) {
+            if (chessBoard->getPiece(i, column) == nullptr){
+                break;
+            }
+            else if (chessBoard->getPiece(i, j)->getColor() != chessBoard->getPiece(row, column)->getColor()) {
                 possibleMoves.push_back(to_string(i)+to_string(j));
                 break;
             }
         }
-    //}
+    }
 
 
     //if black the piece moves forward up the board
-    //if (chessBoard->getPiece(row, column)->getColor() == "b") {
+    if (chessBoard->getPiece(row, column)->getColor() == "b") {
         for (int i = row - 1; (i >=0)&&(i < 8); --i) {
             if (chessBoard->getPiece(i, column) == nullptr){
                 possibleMoves.push_back(to_string(i)+to_string(column));
@@ -49,23 +55,31 @@ vector<string> PawnMove::generatePossibleMoves(int row, int column) {
                 break;
             }
         }
-    //}
+    }
 
     //diagnol up and to the right of the board
-    //if (chessBoard->getPiece(row, column)->getColor() == "b") {
+    if (chessBoard->getPiece(row, column)->getColor() == "b") {
         for (int i = row - 1, j = column + 1; (i >= 0)&&(j < 8); --i, ++j) {
-            if (chessBoard->getPiece(i, j)->getColor() == chessBoard->getPiece(row, column)->getColor()) {
+            if (chessBoard->getPiece(i, column) == nullptr){
+                break;
+            }
+            else if (chessBoard->getPiece(i, j)->getColor() == chessBoard->getPiece(row, column)->getColor()) {
                 possibleMoves.push_back(to_string(i)+to_string(j));
                 break;
             }
         }
-    //}
+    }
 
     //diagnol up and to the left of the board
-    for (int i = row - 1, j = column - 1; (i >= 0)&&(j >= 0); --i, --j) {
-        if (chessBoard->getPiece(i, j)->getColor() == chessBoard->getPiece(row, column)->getColor()) {
-            possibleMoves.push_back(to_string(i)+to_string(j));
-            break;
+    if (chessBoard->getPiece(row, column)->getColor() == "b") {
+        for (int i = row - 1, j = column - 1; (i >= 0)&&(j >= 0); --i, --j) {
+            if (chessBoard->getPiece(i, column) == nullptr){
+                break;
+            }
+            else if (chessBoard->getPiece(i, j)->getColor() == chessBoard->getPiece(row, column)->getColor()) {
+                possibleMoves.push_back(to_string(i)+to_string(j));
+                break;
+            }
         }
     }
 
