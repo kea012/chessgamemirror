e85fbac4a9a5fa9af356fe17e2f6c4ddc87a0c04
L-yz16 <yuzhelin1116@gmail.com>
2024-12-05 01:51:43 -0800

    Added the case where knight moves to an empty square

--- src/KnightMove.cpp
+++ src/KnightMove.cpp
@@ -5,33 +5,93 @@ using namespace std;
 KnightMove::KnightMove(std::string color, Board* chessBoard) : Move(KNIGHTMOVE, color), chessBoard(chessBoard) {}
 
 vector<string> KnightMove::generatePossibleMoves(int row, int column) {
-  if (row+1<8 && column+2<8 && chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(row+1,column+2)->getColor()) {
-    possibleMoves.push_back(to_string(row+1)+to_string(column+2));
-  }
-  if (row+2<8 && column+1<8 && chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(row+2,column+1)->getColor()) {
-    possibleMoves.push_back(to_string(row+2)+to_string(column+1));
-  }
-  if (row+2<8 && column-1>=0 && chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(row+2,column-1)->getColor()) {
-    possibleMoves.push_back(to_string(row+2)+to_string(column-1));
-  }
-  if (row+1<8 && column-2>=0 && chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(row+1,column-2)->getColor()) {
-    possibleMoves.push_back(to_string(row+1)+to_string(column-2));
-  }
-  if (row-1>=0 && column-2>=0 && chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(row-1,column-2)->getColor()) {
-    possibleMoves.push_back(to_string(row-1)+to_string(column-2));
-  }
-  if (row-2>=0 && column-1>=0 && chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(row-2,column-1)->getColor()) {
-    possibleMoves.push_back(to_string(row-2) + std::to_string(column-1));
-  }
-  if (row-2>=0 && column+1<8 && chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(row-2,column+1)->getColor()) {
-    possibleMoves.push_back(to_string(row-2)+to_string(column+1));
-  }
-  if (row-1>=0 && column+2<8 && chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(row-1,column+2)->getColor()) {
-    possibleMoves.push_back(to_string(row-1) + std::to_string(column+2));
+  int newRow = row+1;
+  int newColumn = column+2;
+  if (newRow<8 && newColumn<8) {
+    if (chessBoard->getPiece(newRow,column+2) != nullptr) {
+      if (chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(newRow,newColumn)->getColor())
+        possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+    }
+    else
+      possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
   }
 
-  // if (possibleMoves.size() == 0) {
-  //   possibleMoves.push_back("0");
-  // }
+  newRow = row+2;
+  newColumn = column+1;
+  if (newRow<8 && newColumn<8) {
+    if (chessBoard->getPiece(newRow,column+2) != nullptr) {
+      if (chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(newRow,newColumn)->getColor())
+        possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+    }
+    else
+      possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+  }
+
+  newRow = row+2;
+  newColumn = column-1;
+  if (newRow<8 && newColumn>=0) {
+    if (chessBoard->getPiece(newRow,column+2) != nullptr) {
+      if (chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(newRow,newColumn)->getColor())
+        possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+    }
+    else
+      possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+  }
+
+  newRow = row+1;
+  newColumn = column-2;
+  if (newRow<8 && newColumn>=0) {
+    if (chessBoard->getPiece(newRow,column+2) != nullptr) {
+      if (chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(newRow,newColumn)->getColor())
+        possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+    }
+    else
+      possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+  }
+
+  newRow = row-1;
+  newColumn = column-2;
+  if (newRow>=0 && newColumn>=0) {
+    if (chessBoard->getPiece(newRow,column+2) != nullptr) {
+      if (chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(newRow,newColumn)->getColor())
+        possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+    }
+    else
+      possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+  }
+
+  newRow = row-2;
+  newColumn = column-1;
+  if (newRow>=0 && newColumn>=0) {
+    if (chessBoard->getPiece(newRow,column+2) != nullptr) {
+      if (chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(newRow,newColumn)->getColor())
+        possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+    }
+    else
+      possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+  }
+
+  newRow = row-2;
+  newColumn = column+1;
+  if (newRow>=0 && newColumn<8) {
+    if (chessBoard->getPiece(newRow,column+2) != nullptr) {
+      if (chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(newRow,newColumn)->getColor())
+        possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+    }
+    else
+      possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+  }
+
+  newRow = row-1;
+  newColumn = column+2;
+  if (newRow>=0 && newColumn<8) {
+    if (chessBoard->getPiece(newRow,column+2) != nullptr) {
+      if (chessBoard->getPiece(row,column)->getColor() != chessBoard->getPiece(newRow,newColumn)->getColor())
+        possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+    }
+    else
+      possibleMoves.push_back(to_string(newRow)+to_string(newColumn));
+  }
+
   return possibleMoves;
 }
