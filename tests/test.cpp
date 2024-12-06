e8c5ccde05ece8688ce5215aa025b1d4af81ac1c
L-yz16 <yuzhelin1116@gmail.com>
2024-12-06 13:22:01 -0800

    Added unit tests for Character classes and Move classes

--- tests/test.cpp
+++ tests/test.cpp
@@ -90,23 +90,6 @@ TEST (Board, PrintBoard) {
     EXPECT_EQ(printBoard, board);
 }
 
-
-TEST (QueenMove, MakeQueenMove) {
-    Board* chessBoard = new Board();
-    EXPECT_NO_THROW(QueenMove("w", chessBoard));
-    delete chessBoard;
-}
-
-/*1|♜||♞||♝||♚||♛||♝||♞||♜|
-2|♟||♟||♟||♟||♟||♟||♟||♟|
-3| || || || || || || || |
-4| || || || || || || || |
-5| || || || || || || || |
-6| || || || || || || || |
-7|♙||♙||♙||♙||♙||♙||♙||♙|
-8|♖||♘||♗||♕||♔||♗||♘||♖|
-  A  B  C  D  E  F  G  H*/
-
 TEST(Board, getPiece){
     Board* chessBoard = new Board();
     Rook*  testRook = new Rook("w");
@@ -122,12 +105,209 @@ TEST(Board, movePiece){
     delete chessBoard;
 }
 
-//TEST(Board, setPiece){
-   // Board* chessBoard = newBoard();
-  //  chessBoard->setPiece(6,0, chessBoard->getPiece()
+TEST (QueenMove, MakeQueenMove) {
+    Board* chessBoard = new Board();
+    EXPECT_NO_THROW(QueenMove("w", chessBoard));
+    delete chessBoard;
+}
 
-//1
-//
-//}
+TEST(QueenMove, TestQueenInitialPossibleMove) {
+    Board* chessBoard = new Board();
+    QueenMove queenMove("w", chessBoard);
+    vector<string> moves = queenMove.generatePossibleMoves(0,3);
+    EXPECT_EQ(moves.size(), 0);
+    delete chessBoard;
+}
 
+TEST(QueenMove, TestQueenMoveAtRow4Column4) {
+    Board* chessBoard = new Board();
+    QueenMove queenMove("w", chessBoard);
+    chessBoard->movePiece(0,3,3,3);
+    vector<string> moves = queenMove.generatePossibleMoves(3,3);
+    EXPECT_EQ(moves.size(), 19);
+    delete chessBoard;
+}
 
+TEST(BishopMove, MakeBishopMove) {
+    Board* chessBoard = new Board();
+    EXPECT_NO_THROW(BishopMove("w", chessBoard));
+    delete chessBoard;
+}
+
+TEST(BishopMove, TestBishopInitialPossibleMove) {
+    Board* chessBoard = new Board();
+    BishopMove bishopMove("w", chessBoard);
+    vector<string> moves = bishopMove.generatePossibleMoves(0,2);
+    EXPECT_EQ(moves.size(), 0);
+    delete chessBoard;
+}
+
+TEST(BishopMove, TestBishopMoveAtRow4Column4) {
+    Board* chessBoard = new Board();
+    BishopMove bishopMove("w", chessBoard);
+    chessBoard->movePiece(0,2,3,3);
+    vector<string> moves = bishopMove.generatePossibleMoves(3,3);
+    EXPECT_EQ(moves.size(), 8);
+    delete chessBoard;
+}
+
+TEST(KingMove, MakeKingMove) {
+    Board* chessBoard = new Board();
+    EXPECT_NO_THROW(KingMove("w", chessBoard));
+    delete chessBoard;
+}
+
+TEST(KingMove, TestKingInitialPossibleMove) {
+    Board* chessBoard = new Board();
+    KingMove kingMove("w", chessBoard);
+    vector<string> moves = kingMove.generatePossibleMoves(0,4);
+    EXPECT_EQ(moves.size(), 0);
+    delete chessBoard;
+}
+
+TEST(KingMove, TestCastlingToLeft) {
+    Board* chessBoard = new Board();
+    KingMove kingMove("w", chessBoard);
+    chessBoard->movePiece(0,1,2,2);
+    chessBoard->movePiece(0,2,2,3);
+    chessBoard->movePiece(0,3,2,4);
+    vector<string> moves = kingMove.generatePossibleMoves(0,4);
+    EXPECT_EQ(moves.size(), 2); // should have 03, 02
+    
+    string target = "02"; // castling move
+    bool found = false;
+    for (int i = 0; i < moves.size(); ++i) {
+        if (moves[i] == target)
+            found = true;
+    }
+    EXPECT_TRUE(found);
+    delete chessBoard;
+}
+
+TEST(KingMove, TestCastlingToRight) {
+    Board* chessBoard = new Board();
+    KingMove kingMove("w", chessBoard);
+    chessBoard->movePiece(0,5,2,4);
+    chessBoard->movePiece(0,6,2,5);
+    vector<string> moves = kingMove.generatePossibleMoves(0,4);
+    EXPECT_EQ(moves.size(), 2); // should have 05, 06
+    
+    string target = "06"; // castling move
+    bool found = false;
+    for (int i = 0; i < moves.size(); ++i) {
+        if (moves[i] == target)
+            found = true;
+    }
+    EXPECT_TRUE(found);
+    delete chessBoard;
+}
+
+TEST(KingMove, TestCastlingIfMoved) {
+    Board* chessBoard = new Board();
+    KingMove kingMove("w", chessBoard);
+    chessBoard->movePiece(0,5,2,4);
+    chessBoard->movePiece(0,6,2,5);
+    static_cast<King*>(chessBoard->getPiece(0,4))->setMoved();
+    vector<string> moves = kingMove.generatePossibleMoves(0,4);
+    EXPECT_EQ(moves.size(), 1); // should have 05
+    
+    string target = "06"; // castling move
+    bool found = false;
+    for (int i = 0; i < moves.size(); ++i) {
+        if (moves[i] == target)
+            found = true;
+    }
+    EXPECT_FALSE(found);
+    delete chessBoard;
+}
+
+TEST(KnightMove, MakeKnightMove) {
+    Board* chessBoard = new Board();
+    EXPECT_NO_THROW(KnightMove("w", chessBoard));
+    delete chessBoard;
+}
+
+TEST(KnightMove, TestKnightInitialPossibleMove) {
+    Board* chessBoard = new Board();
+    KnightMove knightMove("w", chessBoard);
+    vector<string> moves = knightMove.generatePossibleMoves(0,1);
+    EXPECT_EQ(moves.size(), 2);
+
+    string target = "20"; // left
+    bool found = false;
+    for (int i = 0; i < moves.size(); ++i) {
+        if (moves[i] == target)
+            found = true;
+    }
+    EXPECT_TRUE(found);
+    target = "22"; // right
+    found = false;
+    for (int i = 0; i < moves.size(); ++i) {
+        if (moves[i] == target)
+            found = true;
+    }
+    EXPECT_TRUE(found);
+    delete chessBoard;
+}
+
+TEST(RookMove, MakeRookMove) {
+    Board* chessBoard = new Board();
+    EXPECT_NO_THROW(RookMove("w", chessBoard));
+    delete chessBoard;
+}
+
+TEST(RookMove, TestRookInitialPossibleMove) {
+    Board* chessBoard = new Board();
+    RookMove rookMove("w", chessBoard);
+    vector<string> moves = rookMove.generatePossibleMoves(0,0);
+    EXPECT_EQ(moves.size(), 0);
+    delete chessBoard;
+}
+
+TEST(RookMove, TestRookMovesAtRow4Column1) {
+    Board* chessBoard = new Board();
+    RookMove rookMove("w", chessBoard);
+    chessBoard->movePiece(0,0,3,0);
+    vector<string> moves = rookMove.generatePossibleMoves(3,0);
+    EXPECT_EQ(moves.size(), 11);
+    delete chessBoard;
+}
+
+TEST(PawnMove, MakePawnMove) {
+    Board* chessBoard = new Board();
+    EXPECT_NO_THROW(PawnMove("w", chessBoard));
+    delete chessBoard;
+}
+
+TEST(PawnMove, TestRookInitialPossibleMove) {
+    Board* chessBoard = new Board();
+    PawnMove pawnMove("w", chessBoard);
+    vector<string> moves = pawnMove.generatePossibleMoves(1,3);
+    EXPECT_EQ(moves.size(), 2);
+    delete chessBoard;
+}
+
+TEST(PawnMove, TestRookMoveAtRow6Column4) {
+    Board* chessBoard = new Board();
+    PawnMove pawnMove("w", chessBoard);
+    chessBoard->movePiece(0,0,5,3);
+    vector<string> moves = pawnMove.generatePossibleMoves(5,3);
+    EXPECT_EQ(moves.size(), 2);
+    
+    string target = "64"; // right
+    bool found = false;
+    for (int i = 0; i < moves.size(); ++i) {
+        if (moves[i] == target)
+            found = true;
+    }
+        EXPECT_TRUE(found);
+
+    target = "62"; // left
+    found = false;
+    for (int i = 0; i < moves.size(); ++i) {
+        if (moves[i] == target)
+            found = true;
+    }
+        EXPECT_TRUE(found);
+    delete chessBoard;
+}
